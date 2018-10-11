90d57b76eb4629ba9cbeb7a794849ac553af8c62
The-R-Win <rwin.esf@gmail.com>
2018-10-11 16:37:56 +0200

    Initial Commit

---
+++ Assignment 3/src/ioPackage/IO.java
@@ -0,0 +1,11 @@
+package ioPackage;
+
+public class IO {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("bla");//asd
+		System.out.println("wut");
+	}
+
+}
